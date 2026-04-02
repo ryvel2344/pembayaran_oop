29499903be64327d7a58c904d6bf88a32478db64
ryvel2344 <i43252645@student.polije.ac.id>
2026-04-02 19:52:04 +0700

    Add files via upload

---
+++ Pembayaran.php
@@ -0,0 +1,32 @@
+<?php
+
+abstract class Pembayaran {
+
+    protected $jumlah;
+
+    public function __construct($jumlah) {
+        $this->jumlah = $jumlah;
+    }
+
+    abstract public function prosesPembayaran();
+
+    public function validasi() {
+        return $this->jumlah > 0;
+    }
+
+    public function diskon() {
+
+        return $this->jumlah * 0.10;
+    }
+
+    public function pajak() {
+
+        return $this->jumlah * 0.11;
+    }
+
+    public function totalBayar() {
+
+        return $this->jumlah - $this->diskon() + $this->pajak();
+    }   
+}
+?>
---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+
+require_once 'TransferBank.php';
+require_once 'Ewallet.php';
+require_once 'QRIS.php';
+require_once 'COD.php';
+require_once 'VirtualAccount.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sistem Pembayaran</title>
+</head>
+
+<body>
+
+<h2>Form Pembayaran</h2>
+
+<form method="POST">
+
+Jumlah :
+
+<input type="number" name="jumlah">
+
+Metode :
+
+<select name="metode">
+
+<option value="transfer">Transfer Bank</option>
+<option value="ewallet">E-Wallet</option>
+<option value="qris">QRIS</option>
+<option value="cod">COD</option>
+<option value="va">Virtual Account</option>
+
+</select>
+
+<button type="submit">Bayar</button>
+
+</form>
+
+<hr>
+
+<?php
+
+if(isset($_POST['jumlah'])){
+
+$jumlah = $_POST['jumlah'];
+$metode = $_POST['metode'];
+
+switch($metode){
+
+case "transfer":
+$obj = new TransferBank($jumlah);
+break;
+
+case "ewallet":
+$obj = new Ewallet($jumlah);
+break;
+
+case "qris":
+$obj = new QRIS($jumlah);
+break;
+
+case "cod":
+$obj = new COD($jumlah);
+break;
+
+case "va":
+$obj = new VirtualAccount($jumlah);
+break;
+
+}
+
+echo $obj->prosesPembayaran();
+
+echo "<br>";
+
+echo $obj->cetakStruk();
+
+echo "<br>";
+
+echo "Diskon : Rp " . $obj->diskon();
+
+echo "<br>";
+
+echo "Pajak : Rp " . $obj->pajak();
+
+echo "<br>";
+
+echo "Total Bayar : Rp " . $obj->totalBayar();
+
+}
+
+?>
+
+</body>
+</html>
